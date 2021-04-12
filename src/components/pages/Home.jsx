e3a15ab69dcd486f9b4a79b456777d63fa96aa5d
Sneha Kumari <snekumar1@publicisgroupe.net>
2021-04-12 13:24:49 +0530

    commit9

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+// import { BannerData } from '../../../server/banners';
 import SliderComponent from "./../SliderComponent";
 import CategoryBanner from "./../CategoryBanner";
 import "../Home.scss";
@@ -7,32 +8,31 @@ import "../Home.scss";
 function Home() {
   const [banner, setBanner] = React.useState([]);
   const [categories, setCategories] = React.useState([]);
-  // React.useEffect(() => {
-  //   axios.get("http://localhost:5000/banners").then(
-  //     (res) => {
-  //       setBanner(res.data);
-  //     },
-  //     (err) => {
-  //       console.error(err);
-  //     }
-  //   );
-  //   axios.get("http://localhost:5000/categories").then(
-  //     (res) => {
-  //       setCategories(res.data);
-  //     },
-  //     (err) => {
-  //       console.error(err);
-  //     }
-  //   );
-  // }, []);
+  React.useEffect(() => {
+    axios.get("/server/banners").then(
+      (res) => {
+        setBanner(res.data);
+      },
+      (err) => {
+        console.error(err);
+      }
+    );
+    axios.get("/server/categories").then(
+      (res) => {
+        setCategories(res.data);
+      },
+      (err) => {
+        console.error(err);
+      }
+    );
+  }, []);
   return (
     <div className="body-section">
-      {/* <SliderComponent banner={banner} />
+      <SliderComponent banner={banner} />
       {categories.map(
         (item) =>
           item.enabled && <CategoryBanner category={item} key={item.key} />
-      )} */}
-      Hi Home
+      )}
     </div>
   );
 }
